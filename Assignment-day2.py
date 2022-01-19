e999476faad6ea4dc5e6c3e84945ae7299c60dfe
RajB123 <91078542+RajB123@users.noreply.github.com>
2022-01-19 16:57:34 +0530

    Assignment-day2 submitted successfully

---
+++ Assignment-day2.py
@@ -0,0 +1,7 @@
+a=1,2,3,4,5
+b=1.2,3.4,5.6,7.8,9.1
+c="John","Ben","Siri","Alexa","Joey"
+d=["Joey",123,2.3]
+e=("Jack",234,3.4)
+f={"name":"Jack", "age": 42}
+g=1,2.3,"Jack",["Jill",123],("Joey",456,45.6),{"name":"Jack", "age": 42}
